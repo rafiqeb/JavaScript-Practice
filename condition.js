fe35f059195d7d9ae33a9c7764ed03d863a81bfd
rafiqeb <mdrafiqulislameb@gmail.com>
2024-08-31 14:57:01 +0600

    added condition.js

---
+++ condition.js
@@ -0,0 +1,99 @@
+// *****  Structure  *****
+//  if (condition/shorto) {
+// console.log('shorto puron hobe') 
+// }
+
+// if, else structure
+// if(condition){
+// console.log('ami ashbo')}
+// else {
+// console.log('ami ashbona')}
+
+// ********       Example      ********
+
+// if(3 < 10){
+//     console.log('I have a diner');
+// }
+
+// var mangoPrice = 250;
+// if(mangoPrice < 300){
+//     console.log('amake ek kegi den')
+// }
+
+// var weight = 40;
+// if(weight <= 20){
+//     console.log('ami nite parbo')
+// }
+// else{
+//     console.log('ami nite parbona')
+// }
+
+// var mangoPrice = 250;
+// if(mangoPrice <= 250){
+//     console.log('Ami kinbo')
+// }
+// else{
+//     console.log('Ami kinbona')
+// }
+
+// ******    && , || er babohar   *****
+
+// const salary = 15000;
+// const isBCS = true;
+// const height = 6.2;
+
+// if(salary > 20000 || isBCS == true && height > 5.7){
+//     console.log('Bia hobe')
+// }
+// else{
+//     console.log('Bia hobe na')
+// }
+
+// ******    Multi lavel condition   *****
+
+// const price = 4000;
+// if(price >= 5000){
+//     const discount = price * 10/100;
+//     const payment = price - discount;
+//     console.log(payment);
+// }
+// else if(price >= 2500){
+//     const discount = price * 5/100;
+//     const payment = price - discount
+//     console.log(payment)
+// }
+// else{
+//     console.log(price)
+// }
+
+// const age = 12;
+// const price = 500;
+// if(age <= 12){
+//     console.log('eates for free')
+// }
+// else if(age <= 16){
+//     const discount = price * 40/100;
+//     const payment = price - discount
+//     console.log(payment)
+// }
+// else if(age >= 60){
+//     const discount = price * 50/100;
+//     const payment = price - discount;
+//     console.log(payment);
+// }
+// else if(age >= 50){
+//     const discount = price * 25/100;
+//     const payment = price - discount
+//     console.log(payment)
+// }
+// else if(age >= 40){
+//     const discount = price * 10/100;
+//     const payment = price - discount
+//     console.log(payment)
+// }
+// else{
+//     console.log(price)
+// }
+
+
+
