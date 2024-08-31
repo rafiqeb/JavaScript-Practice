2a7bc27d097ba06e9b61a89cf2fbb13a18de3d91
rafiqeb <mdrafiqulislameb@gmail.com>
2024-08-31 13:21:29 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,98 @@
+// VARIABLE
+
+// String
+
+// var name = "Rafiqul Islam"
+// var country = "Bangladesh"
+// console.log(name)
+// console.log(country)
+
+// Boolean
+
+// var passed = true;
+// var fail = false;
+// console.log(passed)
+// console.log(fail)
+
+// Number
+
+// var age = 35;
+// var year = 1995;
+// console.log(age)
+// console.log(year)
+
+// Ki dhoroner man
+
+// var adress = "Durlov Pur"
+// console.log(typeof adress)
+
+// jog_biog_gun_vag
+
+// var apple = 240;
+// var orange = 250;
+
+// var apple = 240;
+// // var orange = parseInt('250');
+// var orange = parseFloat('25.5');
+// console.log(apple + orange)
+
+// var first = 0.1;
+// // var first = 0.3;
+// var second = 0.2;
+// var total = first + second;
+// // console.log(total)
+// // console.log(total.toFixed(1))
+// // const num = 50 / 0;        infinity
+// console.log(num)
+
+// Jog_Biog_Goon_Vag
+
+// var price = 35;
+// // price = price + 10;
+// price += 10;    // same to price = price + 10;
+// console.log(price);
+
+// var tv = 15000;
+// tv = tv - 2000;
+// tv -= 2000;      // same to   tv = tv - 2000;
+// console.log(tv)
+
+// var applePrice = 30;
+// applePrice = applePrice * 3;
+// applePrice *= 3;       // same to    applePrice = applePrice * 3;
+// console.log(applePrice)
+
+// var orangePrice = 100;
+// orangePrice = orangePrice / 5;
+// orangePrice /= 5;        //same to      orangePrice = orangePrice / 5;
+// console.log(orangePrice)
+
+// Duiti shonkhar ekti string hole Joger khetre vinno fol ashe but onno khetre ashena
+
+// console.log(40 + '5');
+// console.log(40 - '5')
+// console.log(5 * '5')
+// console.log(50 / '5')
+
+// Homework
+// var total = 1000;
+// var apple = 300;
+// var orange = 400;
+// var payment = apple + orange;
+// console.log(payment)
+// var back = total - payment;
+// console.log(back)
+
+// var english = 75;
+// var bangla = 85;
+// var math = 60;
+// var history = 90;
+// var total = english + bangla + math + history;
+// console.log(total)
+// var sum = total / 4;
+// console.log(sum.toFixed(2))
+
+// var total = 119;
+// var vag = total % 5;
+// console.log(vag);
+
