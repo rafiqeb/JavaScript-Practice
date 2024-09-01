6cc27c7db212fb39971cbfe29e8be6a2a107b7dc
rafiqeb <mdrafiqulislameb@gmail.com>
2024-09-01 11:55:31 +0600

    added array.js

---
+++ array.js
@@ -0,0 +1,25 @@
+//      *********   Array    ******
+
+// const number = [12, 14, 16, 18, 20, 24,];
+// const name = ['Abul', 'Babul', 'Habul', 'Kabul'];
+// console.log(number)
+// console.log(name)
+// console.log(number.length)      var name.length
+// console.log(name.length)
+// console.log(number[7])         var name[3/4/5etc]
+
+// number[3] = 19;
+// console.log(number)       **** number change *****
+
+//        ******** Push Pop   ********
+
+// const number = [12, 14, 20, 24,]
+// console.log(number)
+// number.push(27);
+// console.log(number)
+// number.push(30, 50, 70);
+// console.log(number)
+// number.pop()
+// console.log(number)
+// number.pop()
+// console.log(number)
