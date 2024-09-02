87fdeaa97856936ed69e1ffe6ce475118eb94b39
rafiqeb <mdrafiqulislameb@gmail.com>
2024-09-02 19:47:52 +0600

    added loop.js

---
+++ loop.js
@@ -0,0 +1,92 @@
+//       **********     Loop    *******
+
+// const numbers = [12, 14, 16, 18, 20, 25];
+// console.log(numbers)
+// for(const number of numbers){
+//     console.log(number)
+// }
+
+// const fruits = ['mango', 'orange', 'banana', 'lichi'];
+// console.log(fruits);
+// for (const fruit of fruits) {
+//     console.log(fruit);
+//     console.log('ami khabo')
+// }
+
+//       *********     While Loop     *******
+
+// let number = 0;
+// while(number < 5){
+//     console.log('Ami pabo', number);
+//     // number = number + 1;
+//     number++;
+// }
+
+// let number = 1;
+// while(number <= 60){
+//     console.log('hello', number);
+//     number++;
+// }
+
+// let number = 1;
+// while(number <= 10){
+//     console.log(number);
+//     number++;
+// }
+
+// let number = 50;
+// while(number <= 100){
+//     console.log(number);
+//     number++;
+// }
+
+//     ******* 1-10 Jog Fol    *******
+
+// let number = 1;
+// let sum = 0;
+// while(number <= 10){
+//     console.log(number);
+//     sum = sum + number;
+//     console.log('sum:', sum)
+//     number++;
+// }
+
+//     ******* Even   Number  odd number  ******
+// let number = 1;
+// while(number  <= 10){
+//     console.log(number);
+//     if(number % 2 === 0){
+//         console.log('even number', number)
+//     }
+//     number++;
+// }
+
+// let number = 1;
+// while(number  <= 10){
+//     console.log(number);
+//     if(number % 2 === 1){
+//         console.log('Odd number', number)
+//     }
+//     number++;
+// }
+
+//      *******     Foor Loop      *********
+
+// for(let number = 0; number <= 20; number = number+2){
+//     console.log(number)
+// }
+
+// for(number = 1; number <= 20; number = number+2){
+//     console.log(number)
+// }
+
+// let n = 1;
+// while(n <= 10){
+//     console.log(n);
+//     if(n % 2 === 0){
+//         console.log('even n:', n)
+//     }
+//     n++;
+// }
+
+
